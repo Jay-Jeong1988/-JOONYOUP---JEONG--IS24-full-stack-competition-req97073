51576875f2719805d268e58cf13370573c9870d3
jayjeong <jayjeong1988@gmail.com>
2023-03-27 00:09:35 -0700

    build initial structures

---
+++ client/src/views/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landingPage">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    
+  }
+}
+</script>
